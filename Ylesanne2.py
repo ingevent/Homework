5c56b295ad3a4e87bfa4a0b5601eefb9e75cd9b2
ingevent <56673570+ingevent@users.noreply.github.com>
2019-11-21 08:40:41 +0200

    Exercise 2

---
+++ Ylesanne2.py
@@ -0,0 +1,5 @@
+num = int(input("Sisestage täisarv: "))
+if (num % 2) == 1 or (num) == 6 or (num) == 8 or (num) == 10 or (num) == 12 or (num) == 14 or (num) == 16 or (num) == 18 or (num) == 20 :
+   print(" ei sobi")
+else:
+    print("sobib")
