b3d1e992baa71eb04effd9e09685f4c98c3f3cee
ingevent <56673570+ingevent@users.noreply.github.com>
2019-11-25 23:55:01 +0200

    Exercise 3

---
+++ Ylesanne3.py
@@ -0,0 +1,10 @@
+x= int(input("Palun sisestage esimene arv: "))
+y= int(input("Palun sisestage teine arv: "))
+z= int(input("Palun sisestage kolmas arv: "))
+sum = x + y + z
+if x == y == z:
+    print(x * y * z)
+else:
+    print(sum)
+
+
